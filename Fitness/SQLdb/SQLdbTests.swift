4e1b32fb724b7d76d1901fb366340c42c0052a22
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-12-18 14:01:06 -0500

    Adding SQLite

---
+++ Fitness/SQLdb/SQLdbTests.swift
@@ -0,0 +1,57 @@
+//
+//  SQLdbTests.swift
+//  FitnessTests
+//
+//  Created by Mike Chirico on 12/18/20.
+//
+
+import XCTest
+
+class SQLdbTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testSQLite2() throws {
+            let db = SQLdb()
+            db.open()
+            db.create()
+            
+            guard let  image = db.img(color: UIColor.green,size: CGSize(width: 20,height: 20)) else {
+                print("Can't create image.")
+                XCTAssertTrue(1==2)
+                return
+            }
+            
+            db.insert(data: "data a", image: image, num: 17.8)
+            
+            let r = db.result()
+            
+            XCTAssertTrue(r.count >= 1)
+            for (_ , item) in r.enumerated() {
+                print("\(item.t1key),\t \(item.data), \(item.num), \(item.timeEnter)")
+            }
+            
+            db.close()
+            
+        }
+    
+    
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
--- WidgetFitness/WidgetFitness.swift
+++ WidgetFitness/WidgetFitness.swift
@@ -13,15 +13,15 @@ struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), configuration: ConfigurationIntent())
     }
-
+    
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry = SimpleEntry(date: Date(), configuration: configuration)
         completion(entry)
     }
-
+    
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
-
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
@@ -29,7 +29,7 @@ struct Provider: IntentTimelineProvider {
             let entry = SimpleEntry(date: entryDate, configuration: configuration)
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
@@ -42,16 +42,28 @@ struct SimpleEntry: TimelineEntry {
 
 struct WidgetFitnessEntryView : View {
     var entry: Provider.Entry
-
+    
     var body: some View {
-        Text(entry.date, style: .time)
+        ZStack {
+            VStack {
+                
+                ZStack {
+                    
+                    Color("C0")
+                    
+                    HStack {
+                        Text(entry.date, style: .time)
+                    }
+                }
+            }
+        }
     }
 }
 
 @main
 struct WidgetFitness: Widget {
     let kind: String = "WidgetFitness"
-
+    
     var body: some WidgetConfiguration {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
             WidgetFitnessEntryView(entry: entry)
