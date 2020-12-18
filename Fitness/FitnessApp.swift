3a178a04c3b2ccadab19907a417bf700d592209a
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-12-18 13:27:58 -0500

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Mike Chirico on 12/18/20.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
