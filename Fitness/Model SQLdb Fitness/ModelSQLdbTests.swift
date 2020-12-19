70835c23fedc8f8aaef278cd625f0d474e3c8ea1
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-12-19 09:58:23 -0500

    Clean up

--- Fitness/Model SQLdb Fitness/ModelSQLdbTests.swift
+++ Fitness/Model SQLdb Fitness/ModelSQLdbTests.swift
@@ -8,15 +8,15 @@
 import XCTest
 
 class ModelSQLdbTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() throws {
         
         let db = SQLdb()
@@ -34,12 +34,12 @@ class ModelSQLdbTests: XCTestCase {
         
         db.close()
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
